2110edd7cf0c28d22590e72977016212ea495e21
Art-1985 <arthur19850812@gmail.com>
2019-12-11 10:22:29 +0800

    Create EM85FxxxN_OZ.h

---
+++ Inc/EM85FxxxN_OZ.h
@@ -0,0 +1,30 @@
+//===============================================================================
+//	FILE:			EM85FxxxN_GPIO.h
+//
+//	TITLE:			EM85FxxxN_GPIO Device definitions
+//
+//	Author:			ELAN SRD1 Arthur (Lu Hungche)
+//
+//	Date:			2016/11/21
+//===============================================================================
+#ifndef EM85FxxxN_OZ_H
+#define EM85FxxxN_OZ_H
+
+
+//----------------------------------------------------------------------------
+// Defines for the parameter PinName
+//----------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------
+// Prototypes Of Global Functions
+//----------------------------------------------------------------------------
+void LU_OZ_Table(void);
+void LU_OZ_GPIO(void);
+void LU_OZ_ADC(void);
+void LU_OZ_REF(void);
+void LU_OZ_OSC(void);
+void LU_OZ_PLL(void);
+void LU_OZ_POT(void);
+void LU_OZ_CMP(void);
+
+#endif
